583b18fa367fe51e58ef52987aa413f54f096b18
unknown <madhankumar028@gmail.com>
2017-05-14 21:05:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Author madhankumarj<madhankumar028@gmail.com>
+ */
+(function() {
+
+    'use strict';
+
+    function initMap() {
+        
+        var uluru = {lat: -25.363, lng: 131.044};
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+    }
+}())
