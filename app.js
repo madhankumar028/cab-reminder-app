189b30eba74c5334ed0359ac42668ed22dbe4376
unknown <madhankumar028@gmail.com>
2017-05-27 02:31:16 +0530

    user's current geolocation added

--- app.js
+++ app.js
@@ -5,18 +5,18 @@
 
     'use strict';
 
-    function initMap() {
-        
-        var uluru = {lat: -25.363, lng: 131.044};
-        
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
+    function getUserCurrentLocation(latitude, longitude) {
+        console.log(latitude, longitude);
     }
+
+    function init() {
+      if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+              getUserCurrentLocation(position.coords.latitude
+                                  , position.coords.longitude);
+          });
+      }
+    }
+
+    init();
 }())
